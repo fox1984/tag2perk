376faab4ef9a4ceeee2c59e644808cd495f1209a
kilo <foxstuart@gmail.com>
2025-10-10 10:14:53 -0600

    update view and controller to Accounts

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,16 @@
+# app/controllers/dashboard_controller.rb
 class DashboardController < ApplicationController
-  def show
+  before_action :authenticate_user!
+  
+  def show  # <-- THIS ONE, NOT INDEX
+    # If no current_account, redirect to accounts
+    unless current_account
+      redirect_to accounts_path, alert: "Please select an account first"
+      return
+    end
+    
+    @accounts = current_account
+    @stream_posts = @accounts.stream_posts.order(timestamp: :desc).limit(20)
+    @pending_count = @accounts.stream_posts.where(approved: false).count
   end
 end
