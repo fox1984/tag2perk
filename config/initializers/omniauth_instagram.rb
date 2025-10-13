49ddb695c2786d8af16d25d38c1aee56430203f9
kilo <foxstuart@gmail.com>
2025-10-13 12:14:05 -0600

    add insta bck in for biz monitoring only

--- config/initializers/omniauth_instagram.rb
+++ config/initializers/omniauth_instagram.rb
@@ -1,2 +1,9 @@
-# Load custom Instagram OmniAuth strategy
-require Rails.root.join('lib/omniauth/strategies/instagram')
+# config/initializers/omniauth_instagram.rb
+require Rails.root.join('lib/omniauth/strategies/instagram')  # Correct path
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :instagram,
+           ENV['INSTAGRAM_CLIENT_ID'],
+           ENV['INSTAGRAM_CLIENT_SECRET'],
+           scope: 'user_profile,user_media'
+end
